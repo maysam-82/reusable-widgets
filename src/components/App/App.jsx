75f6e154678f187feb106b33375ae1e8563ba4f6
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-07-25 22:19:08 +1000

    Updated App styles and package.json

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Accordion from '../Accordion/Accordion';
 import { accordionItems } from '../../data/data';
+import './app.css';
 
 const App = () => {
 	return (
-		<div>
+		<div className="ui container">
+			<h3 className="ui header widget-title">Accordion</h3>
 			<Accordion items={accordionItems} />
 		</div>
 	);
