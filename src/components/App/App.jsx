baaa1812d6f298212fe68409c3dd3c81bf2a15f1
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-07-26 20:51:12 +1000

    Created Header component with Link

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,10 +3,12 @@ import Accordion from '../Accordion';
 import Route from '../Route';
 import { accordionItems } from '../../data/data';
 import './app.css';
+import Header from '../Header/Header';
 
 const App = () => {
 	return (
 		<div className="ui container">
+			<Header />
 			<Route path="/">
 				<h3 className="ui header widget-title">Accordion</h3>
 				<Accordion items={accordionItems} />
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from '../Link/Link';
+
+const Header = () => {
+	return (
+		<div>
+			<Link href="/" className="header-item">
+				Accordion
+			</Link>
+		</div>
+	);
+};
+
+export default Header;
