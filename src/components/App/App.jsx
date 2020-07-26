446175ad27508e6a6365670983eed6e2034ceec7
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-07-26 14:29:18 +1000

    Created Route component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import Accordion from '../Accordion/Accordion';
+import Accordion from '../Accordion';
+import Route from '../Route';
 import { accordionItems } from '../../data/data';
 import './app.css';
 
 const App = () => {
 	return (
 		<div className="ui container">
-			<h3 className="ui header widget-title">Accordion</h3>
-			<Accordion items={accordionItems} />
+			<Route path="/">
+				<h3 className="ui header widget-title">Accordion</h3>
+				<Accordion items={accordionItems} />
+			</Route>
 		</div>
 	);
 };
