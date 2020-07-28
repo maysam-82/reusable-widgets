237c52853475cf01e85c388ff5f18ec952394a02
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-07-28 23:14:32 +1000

    Created fetch data with debounce logic on search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export const Search = () => {
 	const [searchTerm, setSearchTerm] = useState('');
+	const [searchResults, setSearchResults] = useState([]);
+
+	useEffect(() => {
+		const search = async () => {
+			const { data } = await axios.get(
+				`http://jsonplaceholder.typicode.com/comments`
+			);
+			setSearchResults(data.filter((item) => item.name.includes(searchTerm)));
+		};
+
+		// debounce logic
+		const timeoutId = setTimeout(() => {
+			if (searchTerm) {
+				search();
+			}
+		}, 500);
+
+		// Return function is returned in the first render of component. As soon as the `searchTerm` is changed, cleanup function is invoked and then the overal function inside `useEffect` gets invoked again.
+		return () => {
+			clearTimeout(timeoutId);
+		};
+	}, [searchTerm]);
+
+	const renderResult = searchResults.map(({ name, id, email }) => {
+		return (
+			<div className="item" key={id}>
+				<div className="content">
+					<div className="header">{name}</div>
+					{email}
+				</div>
+			</div>
+		);
+	});
+
 	return (
 		<div>
 			<div className="ui form">
@@ -14,6 +49,7 @@ export const Search = () => {
 					/>
 				</div>
 			</div>
+			<div className="ui celled list">{renderResult}</div>
 		</div>
 	);
 };
