72a7cba5b4a33d01cbdd461a7e5a6ba043c1b8d9
maysam <maysam82.dev@gmail.com>
2020-11-04 20:02:51 +1100

    Add useViewWidth hook to header to hide timeline

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
+import { useViewWidth } from '../../customHooks/useViewWidth';
 import Link from '../Link/Link';
 import './header.css';
 
 const Header = () => {
-	return (
-		<div className="header-container">
-			<Link href="/" className="ui secondary button">
-				Accordion
-			</Link>
-			<Link href="/search" className="ui secondary button">
-				Search
-			</Link>
-			<Link href="/dropdown" className="ui secondary button">
-				Dropdown
-			</Link>
-			<Link href="/timeline" className="ui secondary button">
-				Timeline
-			</Link>
-		</div>
-	);
+    const isMobileView = useViewWidth('(max-width: 960px)');
+
+    return (
+        <div className="header-container">
+            <Link href="/" className="ui secondary button">
+                Accordion
+            </Link>
+            <Link href="/search" className="ui secondary button">
+                Search
+            </Link>
+            <Link href="/dropdown" className="ui secondary button">
+                Dropdown
+            </Link>
+
+            {!isMobileView ? (
+                <Link href="/timeline" className="ui secondary button">
+                    Timeline
+                </Link>
+            ) : null}
+        </div>
+    );
 };
 
 export default Header;
